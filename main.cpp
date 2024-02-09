587e286b9d7faec5a85ccf69624b88811e7a0a9e
Mark <voloshchuk.mark@chnu.edu.ua>
2024-02-09 11:06:15 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int FirstNumber, SecondNumber;
+    cout<<"Enter first number: ";
+    cin>>FirstNumber;
+    cout<<"Enter second number: ";
+    cin>>SecondNumber;
+    cout<<"And the answer is...";
+    for (int i=0; i<100; i++, cout<<endl)
+    {
+        cout<<".";
+        cout<<".";
+        cout<<".";
+    }
+    cout<<"Bruh";
+    return 0;
+}
